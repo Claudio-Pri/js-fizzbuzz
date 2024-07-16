ef830167a3f95f107df88a6fc49ca87d62eda96b
Claudio-Pri <priv.claudio@gmail.com>
2024-07-16 08:38:12 +0200

    Js 'funzionante'

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,21 @@
 
 for (let i = 1; i <= 100; i++) {
-
     if (i % 3 == 0 && i % 5 == 0) {
         console.log('FizzBuzz');
-
     }
+
+    else if (i % 3 == 0) {
+        console.log('Fizz');
+    }
+
     else if (i % 5 == 0) {
         console.log('Buzz');
-
     }
-    if (i % 3 == 0) {
-        console.log('Fizz');
 
+    else {
+        console.log(i);
     }
-    console.log(i);
 }
-//così non sostituisce i numeri in stampa ma lascritta si posiziona al posto giusto
+
+
 
